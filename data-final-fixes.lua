1a1a5045ccd291e6baafb18f695b9a22768fe8fe
Magne Halvorsen <maghalv@stud.ntnu.no>
2020-03-20 12:04:29 +0100

    ?

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -10,11 +10,35 @@ function inList(data)
     return false
 end
 
+function blacklist(data)
+    -- List of bob's ores
+    list = {
+        "bauxite-ore",
+        "cobalt-ore",
+        "gem-ore",
+        "gold-ore",
+        "lead-ore",
+        "nickel-ore",
+        "quartz",
+        "rutile-ore",
+        "silver-ore",
+        "thorium-ore",
+        "tungsten-ore",
+        "zinc-ore"
+    }
+    for _,item in pairs(list) do
+        if data == item then
+            return true
+        end
+    end
+    return false
+end
+
 data.raw["recipe"]["se-core-fragment-omni"].results = {}
 local products = {}
 
 for _,result in pairs(results) do
-    if inList(result["name"]) then
+    if not blacklist(result["name"]) then
         table.insert(products, result)
     end
 end
